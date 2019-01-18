d98571028b0a58256ca6d5227c24e9d6ac2418f6
edilqueirozdearaujo <projetorgmsp@gmail.com>
2019-01-18 16:39:26 -0200

    versão 2019

---
+++ inc/functions.js
@@ -0,0 +1,100 @@
+
+function BrowserIsPortuguese(){ 
+    var Valor = false;
+	var userLang = navigator.language || navigator.userLanguage;
+	if(userLang == 'pt' || userLang=='pt-BR'){ Valor = true} 
+	return Valor
+}
+
+
+function HrefFromURL(Link,Titulo,Conteudo) {
+	FullLink = HrefFromURLPlus(Link,Titulo,Conteudo,"");
+	return FullLink;
+}
+//Add opção Alvo (target='')
+function HrefFromURLPlus(Link,Classe,Titulo,Conteudo,Alvo) {
+	var TagAlvo = "";
+	if ( Alvo ) {
+		TagAlvo = " target='"+ Alvo +"' ";
+	}
+	FullLink = "<a href='" + Link + "' class='" + Classe + "' title='"+ Titulo + "' " + TagAlvo + " >"+ Conteudo +"</a>";
+	return FullLink;
+}
+//------------------------------------
+//Verifica se mrgControlPolylineMeasure está ativo
+function mrgMedicaoEstaAtiva(){
+	var Ativo = false;
+	var Titulo = $('.polyline-measure-unicode-icon').attr("title");
+	if(Titulo == mrgTxtMeasureTitleOff){Ativo = true}
+	return Ativo
+};
+
+
+//------------------------------------
+function GetLinkGraphhopper(Lat,Lon) {
+	var Link = "https://graphhopper.com/maps/?point=-24.094456588117055%2C-48.364450335502625&point=" + Lat + "%2C" + Lon + "&locale=pt-BR&vehicle=car&weighting=fastest&elevation=true&use_miles=false&layer=OpenStreetMap";
+	return Link;
+}
+function GetLinkMapillary(Lat,Lon,Zoom) {
+	var Link = "http://www.mapillary.com/map/search/" + Lat + "/" + Lon + "/" + Zoom;
+	return Link;
+}
+function GetLinkF4Map(Lat,Lon,Zoom) {
+	var Link = "http://demo.f4map.com/#lat=" + Lat + "&lon=" + Lon + "&zoom="+ Zoom + "&camera.theta=58.465";
+	return Link;
+}
+function GetLinkOSMe(Lat,Lon) {
+	var Link = "http://www.openstreetmap.org/edit#map=18/" + Lat + "/" + Lon;
+	return Link;
+}
+function GetLinkOSMd(Lat,Lon,Zoom) {
+	var Link = "https://www.openstreetmap.org/query?lat="+Lat+"&lon="+Lon+"#map="+Zoom+"/"+Lat+"/"+Lon;
+	//var Link = "http://www.openstreetmap.org/#map=17/" + Lat + "/" + Lon + "&layers=D";
+	return Link;
+}
+function GetLinkNote(Lat,Lon) {
+	var Link = "http://www.openstreetmap.org/note/new#map=14/" + Lat + "/" + Lon;
+	return Link;
+}
+
+function LinkDoMapa(Lat,Lon,Zoom,Dir){	
+	var Host = "http://"+window.location.hostname;
+	var PreLink = Host + Dir + '#' + Zoom + '/' + Lat + '/' + Lon 
+	            + '&mlat=' + Lat + '&mlon=' + Lon
+	var Link    = HrefFromURLPlus(PreLink,"fas fa-external-link-square-alt mrg-button",mrgTxtPermalink,"","");	
+	return Link;
+}
+
+function GerarOpcoesDoMapa(Lat,Lon,Zoom,Dir) {
+	var LinksAlvo = "_blank";
+	var PreLinkGraphhpr  = GetLinkGraphhopper(Lat,Lon); 
+	var PreLinkMapillary = GetLinkMapillary(Lat,Lon,Zoom);
+	var PreLinkF4Map     = GetLinkF4Map(Lat,Lon,Zoom);
+	var PreLinkOSMe      = GetLinkOSMe(Lat,Lon,Zoom);
+	var PreLinkOSMd      = GetLinkOSMd(Lat,Lon,Zoom);
+	
+	var LinkRoute   	= HrefFromURLPlus("#",     "fas fa-map-pin mrg-button","mrgTxtGraphhpr","",LinksAlvo);
+	var LinkGraphhpr  = HrefFromURLPlus(PreLinkGraphhpr,     "fas fa-route mrg-button",mrgTxtGraphhpr,"",LinksAlvo);
+	var LinkMapillary = HrefFromURLPlus(PreLinkMapillary,"fas fa-street-view mrg-button",mrgTxtMapillary,"",LinksAlvo);
+	var LinkF4Map     = HrefFromURLPlus(PreLinkF4Map,    "fas fa-cube mrg-button",mrgTxtF4Map,"",LinksAlvo);
+	var LinkOSMe      = HrefFromURLPlus(PreLinkOSMe,     "fas fa-pen-square  mrg-button",mrgTxtOSMe,"",LinksAlvo);
+	var LinkOSMd      = HrefFromURLPlus(PreLinkOSMd,     "fas fa-search-plus mrg-button",mrgTxtOSMd,"",LinksAlvo);
+
+	PreLinkNote      = GetLinkNote(Lat,Lon); 
+	LinkNote   = HrefFromURLPlus(PreLinkNote,"fas fa-comment mrg-button",mrgTxtNote,"",LinksAlvo);
+		
+	var LinkParaMapa = LinkDoMapa(Lat,Lon,Zoom,Dir);
+	
+	LinksLegenda = "<div class='mrg-button-group'>"
+	             + "<div class='mrg-button'>"+LinkParaMapa+"</div>"
+				    + "<div class='mrg-button'>"+LinkGraphhpr+"</div>"
+	             + "<div class='mrg-button'>"+LinkMapillary+"</div>"
+				    + "<div class='mrg-button'>"+LinkF4Map+"</div>"
+	             + "<div class='mrg-button'>"+LinkOSMe+"</div>"
+				    + "<div class='mrg-button'>"+LinkOSMd+"</div>"
+	             + "<div class='mrg-button'>"+LinkNote+"</div>"
+					 + "<div class='mrg-floatstop'></div>"
+	             + "</div><div class='mrg-floatstop'></div>";
+	
+	return LinksLegenda; 
+}
---
+++ inc/initmap.js
@@ -0,0 +1,73 @@
+var ThunderforestAPIKey = '8c44f9f9817f4c8faeb76a930142683f';
+var mrgMapOnClickAddLock = false;  //Ao clicar surge um marcador. Loqueia para não criar mais.
+var mrgLatLonInicial = [-24.1267,-48.3721]; 
+
+var map = L.map('mapdiv',); //Cria o mapa 
+map.options.maxZoom = 19; 
+map.setView(mrgLatLonInicial, 12);
+
+var hash = new L.Hash(map);
+	
+var attrIBGE = '<a href="https://github.com/tmpsantos/IBGETools" title="IBGETools" >IBGETools</a> | <a href="ftp://geoftp.ibge.gov.br/mapas_estatisticos/censo_2010/mapas_de_setores_censitarios" title="Mapas de Setores Censitários" >Mapas de Setores Censitários (2010)</a> by <a href="http://www.ibge.gov.br/" title="IBGE" >IBGE</a> | hospedado por <a href="https://www.mapbox.com/" title="MapBox" >MapBox</a> &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'; 
+		
+var mrgLayerMapnik = L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
+var mrgLayerMapBoxSat = L.tileLayer.provider('MapBox', {id: 'mapbox.streets-satellite',accessToken: 'pk.eyJ1IjoicHJvamV0b3JnbSIsImEiOiJqeVpremF3In0.SCxZ4ah9ZKxWcELgsKQyWA'});
+var mrgLayerOpenTopoMap = L.tileLayer.provider('OpenTopoMap');
+var mrgLayerStamenToner = L.tileLayer.provider('Stamen.Toner');
+var mrgLayerCycle = L.tileLayer.provider('Thunderforest.OpenCycleMap',{apikey: ThunderforestAPIKey});
+var mrgLayerOutdoors = L.tileLayer.provider('Thunderforest.Outdoors',{apikey: ThunderforestAPIKey});					
+var mrgLayerIBGEr = L.tileLayer('https://{s}.tiles.mapbox.com/v3/tmpsantos.i00mo1kj/{z}/{x}/{y}.png', {attribution: attrIBGE} );
+var mrgLayerIBGEu = L.tileLayer('https://{s}.tiles.mapbox.com/v3/tmpsantos.hgda0m6h/{z}/{x}/{y}.png', {attribution: attrIBGE} );
+
+var mrgActiveLayer = mrgLayerMapnik; //Layer atual inicial
+var mrgSideBySideControlFg = mrgLayerMapnik; //Layer inicial
+var mrgSideBySideControlBk = mrgLayerMapBoxSat; //Layer inicial
+var mrgSideBySideActive = false;
+
+//Marcador que aparece ao clicar no mapa
+var mrgUserTempMarker = L.marker([],{
+                         'draggable' :true
+						});
+												
+//Mostra coordenadas quando clicar em alguma parte do mapa
+function TempMarkerMsg(Lat,Lon,Zoom){
+  var Opcoes = GerarOpcoesDoMapa(Lat,Lon,Zoom,'/mapa/');
+  var Msg = "<div class='mrg-display-latlon'><span class='mrg-fg-blue fas fa-map-marker-alt'></span> "+ mrgTxtMarkerCoord +": <span class='mrg-display-latlon'>"
+          + Lat+',<br> '+ Lon + '</span>'
+	  + '</div>'
+	  + '<p class="">' + Opcoes + '</p>';	
+   return Msg; 
+}
+mrgUserTempMarker.on('click', function(e) {
+   var Zoom = map.getZoom();
+	var Lat = e.latlng.lat;
+	var Lon = e.latlng.lng;
+
+	Lat = Lat.toString().substring(0, 9);
+	Lon = Lon.toString().substring(0, 9);
+
+	var Msg = TempMarkerMsg(Lat,Lon,Zoom);
+	mrgUserTempMarker.bindPopup(Msg); 
+});						
+
+map.on('click', function(e) {
+	if(!mrgMedicaoEstaAtiva()){ //Desativa atividades em caso do controle de medição estar ativo	
+		   var Zoom = map.getZoom();
+			var Lat = e.latlng.lat;
+			var Lon = e.latlng.lng;
+		
+			Lat = Lat.toString().substring(0, 9);
+			Lon = Lon.toString().substring(0, 9);
+			
+			if (!mrgMapOnClickAddLock){
+				mrgMapOnClickAddLock = true;
+				mrgUserTempMarker.setLatLng(e.latlng);
+		        var Msg = TempMarkerMsg(Lat,Lon,Zoom);
+				mrgUserTempMarker.bindPopup(Msg)
+				.addTo(map)
+				.openPopup();
+			}else{mrgMapOnClickAddLock = false;
+				map.removeLayer(mrgUserTempMarker);
+			}
+	}
+});
